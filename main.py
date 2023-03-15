ab007d453d021b4ffd97a0ca70c94a9d01c689c4
MiceX <32219039+shonber@users.noreply.github.com>
2023-03-15 13:42:49 +0200

    15/03/2023_V0.0.1
    15/03/2023_V0.0.1

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+def main(file_name: str):
+    with open(file_name, "w") as fw:
+        for i in range(10001):
+            fw.write(f"{i}\n")
 
-# Project made by Shon Berengard.
-# 4 digit password wordlist.
 
-fb = open("4digit-Bruteforce-wordlist.txt", "w")
-
-for i in range(10):
-    for j in range(10):
-        for k in range(10):
-            for l in range(10):
-                fb.write(str(i)+str(j)+str(k)+str(l)+ '\n')
+if __name__ == "__main__":
+    main("wordlist.txt")
